d33e3d1a3af142c692768dd5a819093f063db622
OkeiDokei <Partlogan00@gmail.com>
2025-11-18 13:22:02 -0500

    Update Kei-dlp.bat
    v3 for archive purposes

--- Kei-dlp.bat
+++ Kei-dlp.bat
@@ -1,14 +1,15 @@
 @echo off
+setlocal enabledelayedexpansion
 
-::Ignore my goofy variable names pls :3
+::Rest In Peace variable names boobs and egg
 
 ::update yt-dlp before start
 yt-dlp.exe -U >NUL 2>&1
+winget install DenoLand.Deno
 
 ::check to make sure setting.txt and debug.txt exist if not create them.
-:prestart2
+:prestart
 	cls
-	setlocal enabledelayedexpansion
 	set counter=0
 
 	if not exist "%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp" (
@@ -19,6 +20,7 @@ yt-dlp.exe -U >NUL 2>&1
 		break>"%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt"
 		(echo=defaultmusic=%HOMEDRIVE%%HOMEPATH%\Music) >> %HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt
 		(echo=defaultvideos=%HOMEDRIVE%%HOMEPATH%\Videos) >> %HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt
+		(echo=cookies=) >> %HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt
 	)
 	
 	if not exist "%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\yt-dlp_Debug.txt" (
@@ -26,13 +28,21 @@ yt-dlp.exe -U >NUL 2>&1
 	)
 	
 	set settingsfile=%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt
-		for /f "tokens=1,2 delims==" %%A in (%settingsfile%) do (
+		for /f "tokens=1,2,3 delims==" %%A in (%settingsfile%) do (
 		set /a counter+=1
 		set "settings[!counter!]=%%B
 	)
 		set settings[
 		set defaultmusic=%settings[1]%
 		set defaultvideos=%settings[2]%
+		set cookies=%settings[3]%
+		
+		
+		
+	set format= null
+	set thumbnail= null
+	set typename= null
+	set type= null
 
 ::Settings menu for script
 :start
@@ -41,13 +51,15 @@ yt-dlp.exe -U >NUL 2>&1
 	echo 2.Change Default Directory Settings
 	echo 3.Open Settings Location
 	echo 4.Exit
+	echo 5.Features to work on
 	
 	CHOICE /C 1234 /M "Enter your choice: "
+		if errorlevel 5 echo cookies form file, loading bar, ascii?, backwards and forwards options, change filter after seleceted,add downloads to arcive.
 		if errorlevel 4 exit
-		if errorlevel 3 start explorer %HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp & goto start
+		if errorlevel 3 start explorer "%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp
 		if errorlevel 2 goto settings
 		if errorlevel 1 goto restart
-
+		
 ::code to write new directorys to setting.txt and update variables based on a token delim parse
 :settings
 	cls
@@ -55,19 +67,34 @@ yt-dlp.exe -U >NUL 2>&1
 	set /p defmusic= "Default Directory For Music: "
 	set /p defvideo= "Default Directory For Videos: "
 	
-	if %defmusic%==[] (
-		set defmusic=%HOMEDRIVE%%HOMEPATH%\Music
-	)
+if "%defmusic%"=="" (
+	set defmusic= %HOMEDRIVE%%HOMEPATH%\Music
+)
+
+if "%defvideo%"=="" (
+	set defvideo= %HOMEDRIVE%%HOMEPATH%\Videos
+)
+
+	cls
+	echo If left blank no cookies will be used.
+	set /p defbrowser= "Please choose a browser supported by yt-dlp: "
 	
-		if %defvideo%==[] (
-		set defmusic=%HOMEDRIVE%%HOMEPATH%\Video
+	if !defbrowser!==[] (
+		set browser=
+		set cookiecode=
+		set cookiebool=No
+	) ELSE (
+		set browser=!defbrowser!
+		set cookiecode=--cookies-from-browser 
+		set cookiebool=Yes
 	)
 	
 	break>%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt
-	(echo=defaultmusic=%defmusic%) >> %HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt
-	(echo=defaultvideos=%defvideo%) >> %HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt
+		(echo=defaultmusic=!defmusic!) >> %HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt
+		(echo=defaultvideos=!defvideo!) >> %HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt
+		(echo=cookies=%cookiecode%%browser%) >> %HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\Settings.txt
 	
-	for /f "tokens=1,2 delims==" %%A in (%settingsfile%) do (
+	for /f "tokens=1,2,3 delims==" %%A in (%settingsfile%) do (
 		set /a counter+=1
 		set "settings[!counter!]=%%B
 	)
@@ -75,11 +102,13 @@ yt-dlp.exe -U >NUL 2>&1
 		set settings[
 		set defaultmusic=%settings[1]%
 		set defaultvideos=%settings[2]%
+		set cookies=%settings[3]%
 		cls
 		echo your defaults are...
 		echo ------------------------------------
-		echo Music: %defaultmusic%
-		echo Videos: %defaultvideos%
+		echo Music: %defmusic%
+		echo Videos: %defvideo%
+		echo Cookies:%cookiebool% Browser:%browser%
 		echo ------------------------------------
 		
 	color 06
@@ -88,10 +117,25 @@ yt-dlp.exe -U >NUL 2>&1
             goto start
         )
 	goto settings
-
-::Start of script
+	
+::Refactored Calls
 :restart
 	cls
+	Call :filetype
+	call :directory
+	call :links
+	call :playlist
+	if "%plysbool%"=="yes" (
+		call :filter
+	) else (
+		set deffilter=
+		set filterCode=
+	)
+	call :fix
+	call :download
+	goto :done
+	
+	
 
 ::Choose File Type
 :filetype
@@ -101,48 +145,23 @@ yt-dlp.exe -U >NUL 2>&1
 	echo 2.mp4
 	
 	CHOICE /C 12 /M "Enter your choice: "
-		if errorlevel 2 goto typemp4
-		if errorlevel 1 goto typemp3
-
-:typemp3
-	set type=-x --audio-format mp3
-	set typename=mp3
-	set thumbnail=--embed-thumbnail^ --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\""
-	goto dirop
-
-:typemp4
-	set type=-S ext:mp4:m4a -S vcodec:avc1
-	set typename= mp4
-	set thumbnail=--embed-thumbnail
-	goto dirop
-
-::Code for fixing type after selected
-:typenew
-	cls
-	color 06
-	echo Choose a diffrent file type.
-	echo 1.mp3
-	echo 2.mp4
-	
-	CHOICE /C 12 /M "Enter your choice: "
-		if errorlevel 2 goto typemp4new
-		if errorlevel 1 goto typemp3new
-
-:typemp3new
-	set type=-x --audio-format mp3
-	set typename=mp3
-	set thumbnail=--embed-thumbnail^ --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\""
-	goto fix
-
-:typemp4new
-	set type=-S ext:mp4:m4a -S vcodec:avc1
-	set typename= mp4
-	set thumbnail=--embed-thumbnail
-	goto fix
+		if errorlevel 2 (
+			set type=-S ext:mp4:m4a -S vcodec:avc1
+			set typename= mp4
+			set thumbnail=--embed-thumbnail
+			exit /b
+		)
+		
+		if errorlevel 1 (
+			set type=-x --audio-format mp3
+			set typename=mp3
+			set "thumbnail=--embed-thumbnail^ --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"""
+			exit /b
+		)
 
 
 ::Choose directory Code
-:dirop
+:directory
 	color 06
 	cls
 	echo Please specify a directory
@@ -151,238 +170,87 @@ yt-dlp.exe -U >NUL 2>&1
 	echo 3.Custom
 	
 	CHOICE /C 123 /M "Enter your choice: "
-		if errorlevel 3 goto dircustom
-		if errorlevel 2 goto dirvideo
-		if errorlevel 1 goto dirmusic
-
-::User Choose custom directory
-:dircustom
-	cls
-	color 02
-	set /p input= "Folder Name: "
-	set boobs=%defaultmusic%
-	set egg="%input%"
+		if errorlevel 3 (
+			cls
+			color 02
+			echo Leaving this blank will select Windows default Download Location
+			set /p input= "Folder Name: "
+			set format="!input!"
 	
-	IF [%input%]==[] goto nodir
-	
-	cls
-	color 06
-	%windir%\System32\choice.exe /m "Open Location?" /c YN
-        if %errorlevel%==1 (
-            start explorer %egg%
-        )
-	goto links
-
-::input variable is empty Error
-:nodir
-	echo ERROR! Please Insert a Directory!
-	pause
-	goto dircustom
-
-::choose music directory
-:dirmusic
-	cls
-	color 02
-	set /p input= "Folder Name: "
-	set boobs=%defaultmusic%
-	set egg="%boobs%\%input%"
-	
-	if [%input%] ==[] (
-		goto nofolder
-	)
-	
-	cls
-	color 06
-    %windir%\System32\choice.exe /m "Open Location?" /c YN
-        if %errorlevel%==1 (
-            start explorer %egg%
-        )
-	goto links
-
-
-::choose video directory
-:dirvideo
-	cls
-	color 02
-	set /p input= "Folder Name: "
-	set boobs=%defaultvideos%
-	set egg="%boobs%\%input%"
-	
-	if [%input%]==[] (
-		goto nofolder
-	)
-	
-	
-	cls
-	color 06
-    %windir%\System32\choice.exe /m "Open Location?" /c YN
-        if %errorlevel%==1 (
-            start explorer %egg%
-        )
-	goto links
-
-::Code for fixing directory after selected
-:dirnew
-	color 06
-	cls
-	echo Please specify a directory
-	echo 1.Default Music
-	echo 2.Default Video
-	echo 3.Custom
-	
-	CHOICE /C 123 /M "Enter your choice: "
-		if errorlevel 3 goto dircustomnew
-		if errorlevel 2 goto dirvideonew
-		if errorlevel 1 goto dirmusicnew
-
-::Code for fixing directory after selected User made
-:dircustomnew
-	cls
-	color 02    
-	set /p input= "Folder Name: "
-	set egg=%input%"
-	IF [%input%]==[] goto nodirnew
-	
-	cls
-	color 06	
-	%windir%\System32\choice.exe /m "Open Location?" /c YN
-		if %errorlevel%==1 (
-			start explorer %egg% 
+				IF [!input!]==[] (
+					echo Downloads Folder Selected
+				)	
 		)
-	goto fix
-
-::Code for fixing directory after selected input variable is empty Error
-:nodirnew
-	echo ERROR! Please Insert a Directory!
-	pause
-	goto dircustomnew
-
-
-::Code for fixing directory after selected input
-:nofoldernew
-	cls
-	color 06
-	set egg=%boobs%
-	%windir%\System32\choice.exe /m "Open Location?" /c YN
-		if %errorlevel%==1 (
-			start explorer %egg%
+		
+		if errorlevel 2 (
+			cls
+			color 02
+			set /p input= "Folder Name: "
+			set format="%defaultvideos%\!input!"
+	
+				if [!input!]==[] (
+					cls
+					echo No Folder
+					pause
+				)
+		exit /b
 		)
-	goto fix
-
-::Code for fixing directory after selected input variable
-:dirmusicnew
-	cls
-	color 02
-	set /p input= "Folder Name: "
-	set boobs="%defaultmusic%
-	set egg="%boobs%\%input%"
+		
+		if errorlevel 1 (
+			cls
+			color 02
+			set /p input= "Folder Name: "
+			set format="%defaultmusic%\!input!"
 	
-	IF [%input%]==[] goto nofoldernew
-	
-	cls
-	color 06
-	%windir%\System32\choice.exe /m "Open Location?" /c YN
-		if %errorlevel%==1 (
-			start explorer %egg%
+			if [!input!] ==[] (
+				cls
+				echo No Folder
+				pause
+			)
+		exit /b
 		)
-	goto fix
+		
 
-::Code for fixing directory after selected input variable
-:dirvideonew
-	cls
-	color 02
-	set /p input= "Folder Name: "
-	set boobs="%defaultvideos%
-	set egg="%boobs%\%input%"
-	
-	IF [%input%]==[] goto nofoldernew
-	
-	cls
-	color 06
-	%windir%\System32\choice.exe /m "Open Location?" /c YN
-		if %errorlevel%==1 (
-			start explorer %egg%
-		(
-	goto fix
 
-::code That opening incorrect directory
-:nofolder
-	cls
-	color 06
-	set egg="%boobs%"
-	%windir%\System32\choice.exe /m "Open Location?" /c YN
-		if %errorlevel%==1 (
-			start explorer %egg%
-		)
-	goto links
 
+		
 ::link code
 :links
 	cls
 	color 0A
 	set /p Link= "Link: "
 	echo Target Link: %Link%
-	goto playlist
-
-::link code to fix after selected
-:linknew
-	cls
-	color 0A
-	set /p Link= "Link: "
-	echo Target Link: %Link%
-	goto fix
+	exit /b
 
 ::playlsit select code
 :playlist
 	cls
 	color 06
 	%windir%\System32\choice.exe /m "Download a Playlist?" /c YN
-		if %errorlevel%==1 goto yes
-		if %errorlevel%==2 goto no
-
-::link code to fix after selected
-:playlistnew
+		if %errorlevel%==1 (
+			set plys=--yes-playlist
+			echo playlist mode: %plys%
+			set plysbool=yes
+			exit /b
+		)
+		if %errorlevel%==2 (
+			set plys=--no-playlist
+			echo playlist mode: %plys%
+			set plysbool=no
+			exit /b
+		)
+:filter
 	cls
-	color 06	
-	%windir%\System32\choice.exe /m "Download a Playlist?" /c YN
-		if %errorlevel%==1 goto yesnew
-		if %errorlevel%==2 goto nonew
-
-::playlsit select code variables
-:yes 
-	set plys=--yes-playlist
-	echo playlist mode: %plys%
-	goto continue
-
-:no
-	set plys=--no-playlist
-	echo playlist mode: %plys%
-	goto continue
-
-:yesnew
-	set plys=--yes-playlist
-	echo playlist mode: %plys%
-	goto fix
-
-:nonew
-	set plys=--no-playlist
-	echo playlist mode: %plys%
-	goto fix
-
-::are you sure confimation window
-:continue
-	cls
-	color 06
-	echo ---- INFO --------------
-	echo File Type is: %typename%
-	echo Target Folder is: %egg%
-	echo Target Link: %link%
-	echo Playlist Mode: %plys%
-	echo ------------------------
-	
-	%windir%\System32\choice.exe /m "is this correct?" /c YN
-		if %errorlevel%==1 goto download
-		if %errorlevel%==2 goto fix
+	color 02
+	echo No Filter Will be applied if left blank.
+	set /p inputfilter= "What To Filter For: "
+		
+	if "%inputfilter%"=="" (
+		set deffilter=
+		set filterCode=
+	) ELSE (
+		set filterCode=--match-filter "_type!=video" --match-filter "title~=(?i)%inputfilter%"
+	)
 
 ::fix incorrect settings code
 :fix
@@ -390,11 +258,13 @@ yt-dlp.exe -U >NUL 2>&1
 	color 06
 	echo ---- INFO --------------
 	echo File Type is: %typename%
-	echo Target Folder is: %egg%
+	echo Target Folder is: %format%
 	echo Target Link: %link%
 	echo Playlist Mode: %plys%
+	echo Filter: %filterCode%
+	echo="yt-dlp.exe -P %format% %type% --add-metadata %Cookies% --compat-options embed-metadata %filterCode% %thumbnail% %plys% %Link%"
 	echo ------------------------
-	echo What would you like to fix?
+	echo Is This Correct? What would you like to fix?
 	echo 1.File Type
 	echo 2.Target Folder
 	echo 3.Link
@@ -402,20 +272,20 @@ yt-dlp.exe -U >NUL 2>&1
 	echo 5.Done
 	
 	CHOICE /C 12345 /M "Enter your choice: "
-		if errorlevel 5 goto continue
-		if errorlevel 4 goto playlistnew
-		if errorlevel 3 goto linknew
-		if errorlevel 2 goto dirnew
-		if errorlevel 1 goto typenew
-
+		if errorlevel 5 goto :download
+		if errorlevel 4 call :playlist
+		if errorlevel 3 call :links
+		if errorlevel 2 call :directory
+		if errorlevel 1 call :type
+	
 ::actual yt-dlp code
 :download
 	cls
 	color 02
 	break>"%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\yt-dlp_Debug.txt"
-	echo Downloading Please Wait....
-	yt-dlp.exe -P %egg% %type% --add-metadata --compat-options embed-metadata %thumbnail% %plys% %Link% >> "%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\yt-dlp_Debug.txt"
-	goto done
+	yt-dlp.exe -P %format% %type% --add-metadata %Cookies% --compat-options embed-metadata %filterCode% %thumbnail% %plys% %Link%
+	pause
+	exit /b
 	
 ::done code exit, etc..
 :done
@@ -423,26 +293,26 @@ yt-dlp.exe -U >NUL 2>&1
 	echo 1.Download More
 	echo 2.Download More Same Settings
 	echo 3.Re-Download
-	echo 4.Settings Info
+	echo 4.Last Downloaded Information
 	echo 5.Exit
 	echo 6.Open Folder
 	echo 7.Open Debug.txt
 	
 	CHOICE /C 1234567 /M "Enter your choice: "
-		if errorlevel 7 goto opendebug
-		if errorlevel 6 goto opendir
-		if errorlevel 5 goto close
-		if errorlevel 4 goto info
-		if errorlevel 3 goto download
-		if errorlevel 2 goto linknew
-		if errorlevel 1 goto restart
+		if errorlevel 7 call :opendebug
+		if errorlevel 6 call :opendir
+		if errorlevel 5 exit
+		if errorlevel 4 call :info
+		if errorlevel 3 call :download
+		if errorlevel 2 call :links
+		if errorlevel 1 goto :restart
 
 :opendebug
 	start explorer "%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\yt-dlp_Debug.txt"
 	goto done
 
 :opendir
-	start explorer %egg%
+	start explorer %format%
 	goto done
 
 :close
@@ -453,23 +323,25 @@ exit
 	color 02
 	echo ---- INFO --------------
 	echo File Type is: %typename%
-	echo Target Folder is: %egg%
+	echo Target Folder is: %format%
 	echo Target Link: %link%
 	echo Playlist Mode: %plys%
+	echo Filter: %filterCode%
+	echo="yt-dlp.exe -P %format% %type% --add-metadata %Cookies% --compat-options embed-metadata %filterCode% %thumbnail% %plys% %Link%"
 	echo ------------------------
 	echo 1.Download More
 	echo 2.Download More Same Settings
 	echo 3.Re-Download
-	echo 4.Hide Info
+	echo 4.Last Downloaded Information
 	echo 5.Exit
 	echo 6.Open Folder
 	echo 7.Open Debug.txt
 	
 	CHOICE /C 1234567 /M "Enter your choice: "
-		if errorlevel 7 goto opendebug
-		if errorlevel 6 goto opendir
-		if errorlevel 5 goto close
-		if errorlevel 4 goto done
-		if errorlevel 3 goto download
-		if errorlevel 2 goto linknew
-		if errorlevel 1 goto restart
+		if errorlevel 7 call :opendebug
+		if errorlevel 6 call :opendir
+		if errorlevel 5 exit
+		if errorlevel 4 call :info
+		if errorlevel 3 call :download
+		if errorlevel 2 call :links
+		if errorlevel 1 goto :restart
