360c4baa8318f604533e934efbd9382c7345908e
OkeiDokei <Partlogan00@gmail.com>
2025-11-18 13:25:06 -0500

    Update Kei-dlp.bat
    v4 for archive purposes

--- Kei-dlp.bat
+++ Kei-dlp.bat
@@ -47,23 +47,38 @@ winget install DenoLand.Deno
 ::Settings menu for script
 :start
 	cls
+	echo=         ❥︎ Kei-dlp! V.4
+	echo= Oops all Meatballs Edition! ( ദ്ദി ˙ᗜ˙ )
+	echo= ------------------------------------------
 	echo 1.Start Script
 	echo 2.Change Default Directory Settings
 	echo 3.Open Settings Location
 	echo 4.Exit
 	echo 5.Features to work on
 	
-	CHOICE /C 1234 /M "Enter your choice: "
-		if errorlevel 5 echo cookies form file, loading bar, ascii?, backwards and forwards options, change filter after seleceted,add downloads to arcive.
+	CHOICE /C 12345 /M "Enter your choice: "
+		if errorlevel 5 call :wip
 		if errorlevel 4 exit
 		if errorlevel 3 start explorer "%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp
 		if errorlevel 2 goto settings
 		if errorlevel 1 goto restart
-		
+
+:wip
+cls
+echo= ❥︎. cookies form file
+echo= ❥︎. loading bar?
+echo= ❥︎. More ascii? 
+echo= ❥︎. backwards and forwards options
+echo= ❥︎. change filter after seleceted
+echo= ❥︎. add downloads to arcive options
+pause
+goto :start
+
 ::code to write new directorys to setting.txt and update variables based on a token delim parse
 :settings
 	cls
 	echo Please set up your default directorys, Inputing nothing will choose Windows default directory.
+	echo= ------------------------------------------------------------------------------------------------
 	set /p defmusic= "Default Directory For Music: "
 	set /p defvideo= "Default Directory For Videos: "
 	
@@ -77,6 +92,7 @@ if "%defvideo%"=="" (
 
 	cls
 	echo If left blank no cookies will be used.
+	echo= ------------------------------------------------------------------------------------------------
 	set /p defbrowser= "Please choose a browser supported by yt-dlp: "
 	
 	if !defbrowser!==[] (
@@ -141,6 +157,7 @@ if "%defvideo%"=="" (
 :filetype
 	color 06
 	echo Choose a file type.
+	echo= --------------------
 	echo 1.mp3
 	echo 2.mp4
 	
@@ -165,6 +182,7 @@ if "%defvideo%"=="" (
 	color 06
 	cls
 	echo Please specify a directory
+	echo= --------------------
 	echo 1.Default Music
 	echo 2.Default Video
 	echo 3.Custom
@@ -174,6 +192,7 @@ if "%defvideo%"=="" (
 			cls
 			color 02
 			echo Leaving this blank will select Windows default Download Location
+			echo= -----------------------------------------------------------------
 			set /p input= "Folder Name: "
 			set format="!input!"
 	
@@ -243,6 +262,7 @@ if "%defvideo%"=="" (
 	cls
 	color 02
 	echo No Filter Will be applied if left blank.
+	echo= ----------------------------------------
 	set /p inputfilter= "What To Filter For: "
 		
 	if "%inputfilter%"=="" (
@@ -284,8 +304,7 @@ if "%defvideo%"=="" (
 	color 02
 	break>"%HOMEDRIVE%%HOMEPATH%\sKeipts\Kei-dlp\yt-dlp_Debug.txt"
 	yt-dlp.exe -P %format% %type% --add-metadata %Cookies% --compat-options embed-metadata %filterCode% %thumbnail% %plys% %Link%
-	pause
-	exit /b
+	goto :done
 	
 ::done code exit, etc..
 :done
